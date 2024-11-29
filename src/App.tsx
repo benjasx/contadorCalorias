23584993a103f3406a990f87248b6584ebc859b7
benjasx <benjasanchez175@gmail.com>
2024-11-29 12:01:36 -0700

    inicio de proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <header className="bg-lime-500 p-5">
+        <div className="max-w-4xl m-auto flex justify-between">
+          <h1 className="uppercase text-white font-black text-xl">contador de calorias</h1>
+          <button className="bg-gray-800 py-1 px-4 rounded text-white hover:bg-slate-600">Reiniciar</button>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
